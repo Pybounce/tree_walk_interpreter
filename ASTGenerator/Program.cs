7fee5cdfd2e390d75cf50696de1509c79ae7cf07
Pybounce <pybounce@protonmail.com>
2025-11-07 01:33:57 +0000

    abstract syntax tree works with a few expressions

--- ASTGenerator/Program.cs
+++ ASTGenerator/Program.cs
@@ -4,20 +4,6 @@ class ASTGenerator
 {
     static void Main(string[] args)
     {
-
-
-            Expr expression = new Expr.Binary(
-        new Expr.Unary(
-            new Token(TokenType.MINUS, "-", null, 1),
-            new Expr.Literal(123)),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Expr.Grouping(
-            new Expr.Literal(45.67)));
-
-        Console.WriteLine(new AstPrinter().Print(expression));
-
-
-        return;
         if (args.Length != 0)
         {
             throw new ArgumentException("Must have exactly zero arguments");
