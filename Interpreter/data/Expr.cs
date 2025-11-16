82471cb746a07ff0729676be5e875bdc0071ec01
Pybounce <pybounce@protonmail.com>
2025-11-16 12:11:19 +0000

    formatting

--- Interpreter/data/Expr.cs
+++ Interpreter/data/Expr.cs
@@ -1,127 +1,152 @@
 public abstract class Expr
 {
-public abstract R Accept<R>(Visitor<R> visitor);
-public interface Visitor<R>
-{
-public R VisitBinaryExpr(Binary expr);
-public R VisitGroupingExpr(Grouping expr);
-public R VisitLiteralExpr(Literal expr);
-public R VisitUnaryExpr(Unary expr);
-public R VisitVariableExpr(Variable expr);
-public R VisitAssignExpr(Assign expr);
-public R VisitLogicalExpr(Logical expr);
-public R VisitCallExpr(Call expr);
-}
-public class Binary: Expr
-{
-public readonly Expr left;
-public readonly Token op;
-public readonly Expr right;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitBinaryExpr(this);
-}
-public Binary(Expr left, Token op, Expr right): base()
-{
-this.left = left;
-this.op = op;
-this.right = right;
-}
-}
-public class Grouping: Expr
-{
-public readonly Expr expression;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitGroupingExpr(this);
-}
-public Grouping(Expr expression): base()
-{
-this.expression = expression;
-}
-}
-public class Literal: Expr
-{
-public readonly Object value;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitLiteralExpr(this);
-}
-public Literal(Object value): base()
-{
-this.value = value;
-}
-}
-public class Unary: Expr
-{
-public readonly Token op;
-public readonly Expr right;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitUnaryExpr(this);
-}
-public Unary(Token op, Expr right): base()
-{
-this.op = op;
-this.right = right;
-}
-}
-public class Variable: Expr
-{
-public readonly Token name;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitVariableExpr(this);
-}
-public Variable(Token name): base()
-{
-this.name = name;
-}
-}
-public class Assign: Expr
-{
-public readonly Token name;
-public readonly Expr value;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitAssignExpr(this);
-}
-public Assign(Token name, Expr value): base()
-{
-this.name = name;
-this.value = value;
-}
-}
-public class Logical: Expr
-{
-public readonly Expr Left;
-public readonly Token Op;
-public readonly Expr Right;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitLogicalExpr(this);
-}
-public Logical(Expr Left, Token Op, Expr Right): base()
-{
-this.Left = Left;
-this.Op = Op;
-this.Right = Right;
-}
-}
-public class Call: Expr
-{
-public readonly Expr Callee;
-public readonly Token Paren;
-public readonly List<Expr> Arguments;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitCallExpr(this);
-}
-public Call(Expr Callee, Token Paren, List<Expr> Arguments): base()
-{
-this.Callee = Callee;
-this.Paren = Paren;
-this.Arguments = Arguments;
-}
-}
+    public abstract R Accept<R>(Visitor<R> visitor);
+
+    public interface Visitor<R>
+    {
+        R VisitBinaryExpr(Binary expr);
+        R VisitGroupingExpr(Grouping expr);
+        R VisitLiteralExpr(Literal expr);
+        R VisitUnaryExpr(Unary expr);
+        R VisitVariableExpr(Variable expr);
+        R VisitAssignExpr(Assign expr);
+        R VisitLogicalExpr(Logical expr);
+        R VisitCallExpr(Call expr);
+    }
+
+    public class Binary : Expr
+    {
+        public readonly Expr left;
+        public readonly Token op;
+        public readonly Expr right;
+
+        public Binary(Expr left, Token op, Expr right) : base()
+        {
+            this.left = left;
+            this.op = op;
+            this.right = right;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitBinaryExpr(this);
+        }
+    }
+
+    public class Grouping : Expr
+    {
+        public readonly Expr expression;
+
+        public Grouping(Expr expression) : base()
+        {
+            this.expression = expression;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitGroupingExpr(this);
+        }
+    }
+
+    public class Literal : Expr
+    {
+        public readonly Object value;
+
+        public Literal(Object value) : base()
+        {
+            this.value = value;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitLiteralExpr(this);
+        }
+    }
+
+    public class Unary : Expr
+    {
+        public readonly Token op;
+        public readonly Expr right;
+
+        public Unary(Token op, Expr right) : base()
+        {
+            this.op = op;
+            this.right = right;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitUnaryExpr(this);
+        }
+    }
+
+    public class Variable : Expr
+    {
+        public readonly Token name;
+
+        public Variable(Token name) : base()
+        {
+            this.name = name;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitVariableExpr(this);
+        }
+    }
+
+    public class Assign : Expr
+    {
+        public readonly Token name;
+        public readonly Expr value;
+
+        public Assign(Token name, Expr value) : base()
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitAssignExpr(this);
+        }
+    }
+
+    public class Logical : Expr
+    {
+        public readonly Expr Left;
+        public readonly Token Op;
+        public readonly Expr Right;
+
+        public Logical(Expr Left, Token Op, Expr Right) : base()
+        {
+            this.Left = Left;
+            this.Op = Op;
+            this.Right = Right;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitLogicalExpr(this);
+        }
+    }
+
+    public class Call : Expr
+    {
+        public readonly Expr Callee;
+        public readonly Token Paren;
+        public readonly List<Expr> Arguments;
+
+        public Call(Expr Callee, Token Paren, List<Expr> Arguments) : base()
+        {
+            this.Callee = Callee;
+            this.Paren = Paren;
+            this.Arguments = Arguments;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitCallExpr(this);
+        }
+    }
 }
--- Interpreter/data/Stmt.cs
+++ Interpreter/data/Stmt.cs
@@ -1,112 +1,134 @@
 public abstract class Stmt
 {
-public abstract R Accept<R>(Visitor<R> visitor);
-public interface Visitor<R>
-{
-public R VisitExpressionStmt(Expression stmt);
-public R VisitVarStmt(Var stmt);
-public R VisitBlockStmt(Block stmt);
-public R VisitIfStmt(If stmt);
-public R VisitWhileStmt(While stmt);
-public R VisitFunctionStmt(Function stmt);
-public R VisitReturnStmt(Return stmt);
-}
-public class Expression: Stmt
-{
-public readonly Expr expression;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitExpressionStmt(this);
-}
-public Expression(Expr expression): base()
-{
-this.expression = expression;
-}
-}
-public class Var: Stmt
-{
-public readonly Token name;
-public readonly Expr initialiser;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitVarStmt(this);
-}
-public Var(Token name, Expr initialiser): base()
-{
-this.name = name;
-this.initialiser = initialiser;
-}
-}
-public class Block: Stmt
-{
-public readonly List<Stmt> statements;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitBlockStmt(this);
-}
-public Block(List<Stmt> statements): base()
-{
-this.statements = statements;
-}
-}
-public class If: Stmt
-{
-public readonly Expr Condition;
-public readonly Stmt ThenBranch;
-public readonly Stmt? ElseBranch;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitIfStmt(this);
-}
-public If(Expr Condition, Stmt ThenBranch, Stmt? ElseBranch): base()
-{
-this.Condition = Condition;
-this.ThenBranch = ThenBranch;
-this.ElseBranch = ElseBranch;
-}
-}
-public class While: Stmt
-{
-public readonly Expr Condition;
-public readonly Stmt Body;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitWhileStmt(this);
-}
-public While(Expr Condition, Stmt Body): base()
-{
-this.Condition = Condition;
-this.Body = Body;
-}
-}
-public class Function: Stmt
-{
-public readonly Token Name;
-public readonly List<Token> Params;
-public readonly List<Stmt> Body;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitFunctionStmt(this);
-}
-public Function(Token Name, List<Token> Params, List<Stmt> Body): base()
-{
-this.Name = Name;
-this.Params = Params;
-this.Body = Body;
-}
-}
-public class Return: Stmt
-{
-public readonly Token Keyword;
-public readonly Expr expression;
-public override R Accept<R>(Visitor<R> visitor)
-{
-return visitor.VisitReturnStmt(this);
-}
-public Return(Token Keyword, Expr expression): base()
-{
-this.Keyword = Keyword;
-this.expression = expression;
-}
-}
+    public abstract R Accept<R>(Visitor<R> visitor);
+
+    public interface Visitor<R>
+    {
+        R VisitExpressionStmt(Expression stmt);
+        R VisitVarStmt(Var stmt);
+        R VisitBlockStmt(Block stmt);
+        R VisitIfStmt(If stmt);
+        R VisitWhileStmt(While stmt);
+        R VisitFunctionStmt(Function stmt);
+        R VisitReturnStmt(Return stmt);
+    }
+
+    public class Expression : Stmt
+    {
+        public readonly Expr expression;
+
+        public Expression(Expr expression) : base()
+        {
+            this.expression = expression;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitExpressionStmt(this);
+        }
+    }
+
+    public class Var : Stmt
+    {
+        public readonly Token name;
+        public readonly Expr initialiser;
+
+        public Var(Token name, Expr initialiser) : base()
+        {
+            this.name = name;
+            this.initialiser = initialiser;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitVarStmt(this);
+        }
+    }
+
+    public class Block : Stmt
+    {
+        public readonly List<Stmt> statements;
+
+        public Block(List<Stmt> statements) : base()
+        {
+            this.statements = statements;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitBlockStmt(this);
+        }
+    }
+
+    public class If : Stmt
+    {
+        public readonly Expr Condition;
+        public readonly Stmt ThenBranch;
+        public readonly Stmt? ElseBranch;
+
+        public If(Expr Condition, Stmt ThenBranch, Stmt? ElseBranch) : base()
+        {
+            this.Condition = Condition;
+            this.ThenBranch = ThenBranch;
+            this.ElseBranch = ElseBranch;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitIfStmt(this);
+        }
+    }
+
+    public class While : Stmt
+    {
+        public readonly Expr Condition;
+        public readonly Stmt Body;
+
+        public While(Expr Condition, Stmt Body) : base()
+        {
+            this.Condition = Condition;
+            this.Body = Body;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitWhileStmt(this);
+        }
+    }
+
+    public class Function : Stmt
+    {
+        public readonly Token Name;
+        public readonly List<Token> Params;
+        public readonly List<Stmt> Body;
+
+        public Function(Token Name, List<Token> Params, List<Stmt> Body) : base()
+        {
+            this.Name = Name;
+            this.Params = Params;
+            this.Body = Body;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitFunctionStmt(this);
+        }
+    }
+
+    public class Return : Stmt
+    {
+        public readonly Token Keyword;
+        public readonly Expr expression;
+
+        public Return(Token Keyword, Expr expression) : base()
+        {
+            this.Keyword = Keyword;
+            this.expression = expression;
+        }
+
+        public override R Accept<R>(Visitor<R> visitor)
+        {
+            return visitor.VisitReturnStmt(this);
+        }
+    }
 }
